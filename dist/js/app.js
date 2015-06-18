550b8ff3f0a0373a096b483ed17ff1d1e9978786
Mark Harper <markharper20@gmail.com>
2015-06-18 12:54:34 -0400

    functional view and model

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,12 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var Backbone = require('backbone');
+var $ = require('jquery');
+
 MasonsCoolCustomModelConstructor = Backbone.Model.extend({
 
   initialize: function() {
     console.log('wow, a function was born!');
-  }
+  },
 
 });
 
@@ -17,13 +20,45 @@ var coolInstance = new MasonsCoolCustomModelConstructor({
   name: "Mason",
   cool: false
 });
-  
+
 var collectionInstance = new AnExampleCollection();
 
-collectionInstance.fetch().done(function(){
-  console.log('the first thing in collectionInstance after fetching is', collectionInstance.first());
+collectionInstance.add(coolInstance);
+
+console.log(coolInstance.get('name'));
+
+coolInstance.set('name', 'mark');
+
+collectionInstance.set(coolInstance);
+
+console.log(coolInstance.get('name'));
+
+collectionInstance.remove(coolInstance);
+
+console.log(collectionInstance.get(coolInstance));
+
+
+var MarkView = Backbone.View.extend({
+
+  template: function(model){
+    return "Hi, I'm a man named " + model.get('name');
+  },
+
+  initialize: function(){
+    $('body').append(this.el);
+    this.render();
+  },
+
+  render: function(){
+    this.$el.html( this.template(this.model) )
+  }
+
 });
-},{}]},{},[1])
+
+new MarkView({model: coolInstance});
+
+
+},{"backbone":"backbone","jquery":"jquery"}]},{},[1])
 
 
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+
 MasonsCoolCustomModelConstructor = Backbone.Model.extend({
 
   initialize: function() {
     console.log('wow, a function was born!');
-  }
+  },
 
 });
 
@@ -16,9 +19,40 @@ var coolInstance = new MasonsCoolCustomModelConstructor({
   name: "Mason",
   cool: false
 });
-  
+
 var collectionInstance = new AnExampleCollection();
 
-collectionInstance.fetch().done(function(){
-  console.log('the first thing in collectionInstance after fetching is', collectionInstance.first());
+collectionInstance.add(coolInstance);
+
+console.log(coolInstance.get('name'));
+
+coolInstance.set('name', 'mark');
+
+collectionInstance.set(coolInstance);
+
+console.log(coolInstance.get('name'));
+
+collectionInstance.remove(coolInstance);
+
+console.log(collectionInstance.get(coolInstance));
+
+
+var MarkView = Backbone.View.extend({
+
+  template: function(model){
+    return "Hi, I'm a man named " + model.get('name');
+  },
+
+  initialize: function(){
+    $('body').append(this.el);
+    this.render();
+  },
+
+  render: function(){
+    this.$el.html( this.template(this.model) )
+  }
+
 });
+
+new MarkView({model: coolInstance});
+
